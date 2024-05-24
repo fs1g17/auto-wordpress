e3d94379ab94a035e8620f7012eb6ae921e0499e
fs1g17 <fs1g17@soton.ac.uk>
2024-05-24 18:42:12 +0100

    feat(self healing): script that only executes IF stuff works

--- init2.sh
+++ init2.sh
@@ -4,17 +4,14 @@ sudo -u www-data wp core install --url="$URL" --title="$TITLE" --admin_user="$AD
 
 sudo systemctl restart apache2
 
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot certonly --non-interactive --agree-tos --register-unsafely-without-email -d $WORDPRESS_DOMAIN --webroot -w /var/www/html/
-
+sudo snap install --classic certbot && \
+sudo ln -s /snap/bin/certbot /usr/bin/certbot && \
+sudo certbot certonly --non-interactive --agree-tos --register-unsafely-without-email -d $WORDPRESS_DOMAIN --webroot -w /var/www/html/ && \
 # env subst WORDPRESS_DOMAIN
-sed -i "s/\${WORDPRESS_DOMAIN}/$WORDPRESS_DOMAIN/g" /home/wlspro/source/wordpress-ssl.conf
-
+sed -i "s/\${WORDPRESS_DOMAIN}/$WORDPRESS_DOMAIN/g" /home/wlspro/source/wordpress-ssl.conf && \
 # Copy ssl conf file
-sudo cp /home/wlspro/source/wordpress-ssl.conf /etc/apache2/sites-available/wordpress-ssl.conf
-
+sudo cp /home/wlspro/source/wordpress-ssl.conf /etc/apache2/sites-available/wordpress-ssl.conf && \
 # activate the ssl site and restart apache
-sudo a2enmod ssl
-sudo a2ensite wordpress-ssl.conf
+sudo a2enmod ssl && \
+sudo a2ensite wordpress-ssl.conf && \
 sudo service apache2 restart
--- init3.sh
+++ init3.sh
@@ -1,9 +1,11 @@
 cd /var/www/html
 
-sudo a2dissite wordpress-ssl.conf
-sudo service apache2 restart
-
-sudo certbot certonly --non-interactive --agree-tos --register-unsafely-without-email -d $WORDPRESS_DOMAIN --webroot -w /var/www/html/
-sudo a2ensite wordpress-ssl.conf
-
+sudo certbot certonly --non-interactive --agree-tos --register-unsafely-without-email -d $WORDPRESS_DOMAIN --webroot -w /var/www/html/ && \
+# env subst WORDPRESS_DOMAIN
+sed -i "s/\${WORDPRESS_DOMAIN}/$WORDPRESS_DOMAIN/g" /home/wlspro/source/wordpress-ssl.conf && \
+# Copy ssl conf file
+sudo cp /home/wlspro/source/wordpress-ssl.conf /etc/apache2/sites-available/wordpress-ssl.conf && \
+# activate the ssl site and restart apache
+sudo a2enmod ssl && \
+sudo a2ensite wordpress-ssl.conf && \
 sudo service apache2 restart
